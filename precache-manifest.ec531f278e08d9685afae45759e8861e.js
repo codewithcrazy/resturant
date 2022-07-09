fcde4988599a990b4a76f9c85b962a88e993de99
CodeWithCrazy <90599826+codewithcrazy@users.noreply.github.com>
2022-07-10 01:53:39 +0530

    Add files via upload

---
+++ precache-manifest.ec531f278e08d9685afae45759e8861e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "55743d15e8e53f09cc36",
+    "url": "/resturant/static/css/main.fa7c9fe5.chunk.css"
+  },
+  {
+    "revision": "55743d15e8e53f09cc36",
+    "url": "/resturant/static/js/main.55743d15.chunk.js"
+  },
+  {
+    "revision": "12a547d5be2359b88d04",
+    "url": "/resturant/static/js/1.12a547d5.chunk.js"
+  },
+  {
+    "revision": "9a10cb8536e92ce2e653",
+    "url": "/resturant/static/js/runtime~main.9a10cb85.js"
+  },
+  {
+    "revision": "88b062b111cd55bcbe2ad0d5e365cb4d",
+    "url": "/resturant/index.html"
+  }
+];
